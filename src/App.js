e62cc78b347f8c8f3b0241f242f0473b894e3650
egarinad <zavadskij76@gmail.com>
2022-05-21 16:57:54 +0300

    yep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./scss/_base.scss";
+import "./scss/input.scss";
+import gitHubImage from "./img/Vector.png";
+import searchImage from "./img/search-image.png";
+import searchSmall from "./img/small-search.png";
+import axios from "axios";
 
-function App() {
+import Output from "./components/Output";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [repos, setRepos] = useState([]);
+  const [info, setInfo] = useState([]);
+  const [isFind, setFind] = useState(true)
+
+  const [loading, setLoading] = useState(false);
+  const [isSearch, setSearch] = useState(false);
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      setSearch(true);
+      setLoading(true);
+      try {
+        const result_rep = await axios(
+          `https://api.github.com/users/${input}/repos`
+        );
+        const result_info = await axios(
+          `https://api.github.com/users/${input}`
+        );
+        setFind(true);
+        setRepos(result_rep.data);
+        setInfo(result_info.data);
+        setLoading(false);
+        console.log(result_rep.data);
+      } catch (err) {
+        setFind(false)
+        setLoading(false);
+        console.log(err);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  // console.log(repos)
+  // console.log(repos.length)
+  //console.log(repos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="input-bar">
+        <div className="input-bar-container">
+          <a href="https://github.com/" target="_blank">
+            <img src={gitHubImage} className="input-bar__github-image" alt="" />
+          </a>
+          <div className="input-bar__box box">
+            <img className="box__image" src={searchSmall} alt="" />
+            <input
+              type="text"
+              className="box__input"
+              placeholder="Enter GitHub username"
+              onChange={handleChange}
+              value={input}
+              onKeyPress={search}
+            />
+          </div>
+        </div>
+      </div>
+
+
+
+      {isSearch ? (
+            <Output info={info} repos={repos} loading={loading} isFind={isFind}/>
+      ) : (
+        <div className="search">
+          <img className="search__image" src={searchImage} alt="" />
+          <div className="search__text">Start with searching a GitHub user</div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Output.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../scss/output.scss";
+import user from "../img/usernf.png"
+import About from "./About";
+import Result from "./Result";
+import searchImage from "../img/search-image.png";
+
+const Output = ({ info ,repos, loading, isFind }) => {
+
+
+
+  // const listRepos =
+  //      repos.length !== 0 ? (
+  //           repos.map((item) => (
+  //           <li className="rep" key={item.id}>
+  //                 {item.name}
+  //           </li>
+  //           ))
+  //   ) :
+  //   (
+  //     <li>No repos</li>
+  //   );
+
+  if (loading) {
+      return (
+            <div className="output-loader">
+                  <div className="loader"></div>
+            </div>
+      );
+  }
+
+  return (
+        <div className="output">
+              {isFind===true ? (
+                    <div className="output-container">
+                        <About info={info}/>
+                        <Result repos={repos}/>
+                    </div>
+              ) :
+              (
+                    <div className="not-found">
+                          <div className="not-found-container">
+                              <img className="not-found__image" src={user} alt="" />
+                              <div className="not-found__text">User not found</div>
+                          </div>
+                    </div>
+              ) }
+        </div>
+  );
+};
+
+export default Output;
